8efae92314588bf785388106b39a6e78fa44f34c
Gustav Behm <me@rootmos.io>
2021-06-06 10:45:55 +0200

    Wait for sound before recording

--- linux/trec/src/trec.c
+++ linux/trec/src/trec.c
@@ -12,24 +12,81 @@ struct options {
     const char* device;
     unsigned int channels;
     unsigned int rate;
+
+    float lead_in_seconds;
+    float threshold_percent;
 };
 
 struct state {
     int running;
+    int recording;
+
     int sfd;
 
     pid_t enc_pid;
     int enc_fd;
 
     snd_pcm_t* pcm;
+    unsigned int channels;
 
     void* buf;
     size_t f, fi, fj, fn, r;
+
+    size_t lead_in_frames;
+    uint16_t threshold;
+    uint16_t max;
 };
+
+static void monitor_init(struct state* st, const struct options* opts)
+{
+    st->lead_in_frames = opts->lead_in_seconds * opts->rate * opts->channels;
+    if(st->lead_in_frames > st->fn) {
+        failwith("buffer too small for %f seconds of lead in", opts->lead_in_seconds);
+    }
+    st->threshold = opts->threshold_percent/100 * INT16_MAX;
+    st->max = 0;
+}
+
+static void monitor_handle_new_frames(struct state* st,
+                                      const void* buf, snd_pcm_uframes_t n)
+{
+    const int16_t* samples = buf;
+    for(size_t i = 0; i < n * st->channels; i++) {
+        if(samples[i] > st->max) {
+            st->max = samples[i];
+        }
+    }
+}
+
+static int monitor_check_for_sound(struct state* st)
+{
+    if(st->max > st->threshold) {
+        return 1;
+    }
+
+    if(st->fi <= st->fj) {
+        size_t n = st->fj - st->fi;
+        if(n > st->lead_in_frames) {
+            st->fi = st->fj - st->lead_in_frames;
+        }
+    } else {
+        size_t n = st->fn - st->fi;
+        if(n + st->fj > st->lead_in_frames) {
+            if(st->lead_in_frames <= st->fj) {
+                st->fi = st->fj - st->lead_in_frames;
+            } else {
+                st->fi += st->lead_in_frames - st->fj;
+            }
+        }
+    }
+
+    return 0;
+}
 
 static void alsa_init(struct state* st, const struct options* opts)
 {
-    st->fn = 1 /* seconds */ * opts->rate;
+    st->fn = 5 /* seconds */ * opts->rate;
+    st->channels = opts->channels;
 
     int r = snd_pcm_open(&st->pcm, opts->device,
                          SND_PCM_STREAM_CAPTURE, SND_PCM_NONBLOCK);
@@ -38,13 +95,13 @@ static void alsa_init(struct state* st, const struct options* opts)
     r = snd_pcm_set_params(st->pcm,
                            SND_PCM_FORMAT_S16_LE,
                            SND_PCM_ACCESS_RW_INTERLEAVED,
-                           opts->channels,
+                           st->channels,
                            opts->rate,
                            0, /* soft resample */
                            0); /* required latency */
     CHECK_ALSA(r, "snd_pcm_set_params");
 
-    st->f = sizeof(int16_t)*opts->channels;
+    st->f = sizeof(int16_t)*st->channels;
     size_t n = st->f * st->fn;
     st->buf = malloc(n); CHECK_MALLOC(st->buf);
     st->fi = st->fj = st->r = 0;
@@ -83,6 +140,8 @@ static void alsa_handle_read(struct state* st)
         CHECK_ALSA(s, "snd_pcm_readi");
 
         trace("captured %ld frames (buffer %zu)", s, n);
+        monitor_handle_new_frames(st, buf, s);
+
         st->fj += s;
         if(st->fj == st->fn) {
             st->fj = 0;
@@ -95,7 +154,13 @@ static int buffer_non_empty(struct state* st)
     return st->fi != st->fj;
 }
 
-static void encoder_init(struct state* st, const struct options* opts)
+static void encoder_init(struct state* st)
+{
+    st->enc_pid = -1;
+    st->enc_fd = -1;
+}
+
+static void encoder_start(struct state* st, const struct options* opts)
 {
     int fds[2];
     int r = pipe(fds); CHECK(r, "pipe");
@@ -171,12 +236,15 @@ static void encoder_write(struct state* st)
 
 static void encoder_deinit(struct state* st)
 {
-    set_blocking(st->enc_fd, 1);
-    encoder_write(st);
+    if(st->enc_fd >= 0) {
+        set_blocking(st->enc_fd, 1);
+        encoder_write(st);
+        int r = close(st->enc_fd); CHECK(r, "close");
+    }
 
-    int r = close(st->enc_fd); CHECK(r, "close");
-
-    pid_t p = waitpid(st->enc_pid, NULL, 0); CHECK(p, "waitpid");
+    if(st->enc_pid >= 0) {
+        pid_t p = waitpid(st->enc_pid, NULL, 0); CHECK(p, "waitpid");
+    }
 }
 
 static void signalfd_init(struct state* st)
@@ -228,6 +296,9 @@ void parse_opts(struct options* opts, int argc, char* argv[])
     opts->channels = 2;
     opts->rate = 44100;
 
+    opts->lead_in_seconds = 0.2;
+    opts->threshold_percent = 1.0;
+
     if(argc < 2) {
         failwith("too few arguments");
     }
@@ -243,14 +314,18 @@ int main(int argc, char* argv[])
 
     struct state st = {
         .running = 1,
+        .recording = 0,
+        .max = 0,
     };
 
     alsa_init(&st, &opts);
     signalfd_init(&st);
-    encoder_init(&st, &opts);
+    monitor_init(&st, &opts);
+    encoder_init(&st);
 
     alsa_start(&st);
 
+    info("waiting");
     while(st.running) {
         const int afd = snd_pcm_poll_descriptors_count(st.pcm);
         CHECK_ALSA(afd, "snd_pcm_poll_descriptors_count");
@@ -260,7 +335,7 @@ int main(int argc, char* argv[])
         fds[0] = (struct pollfd) { .fd = st.sfd, .events = POLLIN };
         fds[1] = (struct pollfd) { .fd = st.enc_fd, .events = 0 };
 
-        if(buffer_non_empty(&st)) {
+        if(st.recording && buffer_non_empty(&st)) {
             fds[1].events |= POLLOUT;
         }
 
@@ -294,6 +369,14 @@ int main(int argc, char* argv[])
 
         if(revents & POLLIN) {
             alsa_handle_read(&st);
+        }
+
+        if(!st.recording) {
+            if(monitor_check_for_sound(&st)) {
+                info("recording");
+                encoder_start(&st, &opts);
+                st.recording = 1;
+            }
         }
     }
 
